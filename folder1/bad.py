3a8e4ebccdd4ce822395bfa8a3ee366694579c3d
praveen200011 <149142906+praveen200011@users.noreply.github.com>
2025-05-16 15:41:58 +0530

    Update bad.py

---
+++ folder1/bad.py
@@ -0,0 +1,8 @@
+def addtwonumber(num1, num2):
+    return num1+num2
+
+num1 = input ("enter the number 1")
+num2 = input ("enter the number 2")
+
+result=addtwonumber(num1, num2)
+print("the sum is:{result}")
